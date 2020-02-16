28f460122bbaf53fabb5d17d872603c435dd55e6
Andrew Milne <andrew+github@milne.tel>
2020-02-16 13:18:27 +0000

    Remove licence screen from installer. Build release binaries for v1.0.1

--- CreatePhotosFolder.App/Properties/AssemblyInfo.cs
+++ CreatePhotosFolder.App/Properties/AssemblyInfo.cs
@@ -1,18 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CreatePhotosFolder")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CreatePhotosFolder")]
+[assembly: AssemblyTitle("CreatePhotosFolder.App")]
+[assembly: AssemblyCompany("Andrew Milne")]
+[assembly: AssemblyProduct("Create Photos Folder")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,15 +14,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4083a609-5d90-4fb1-b0b8-241240c3ffdb")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
